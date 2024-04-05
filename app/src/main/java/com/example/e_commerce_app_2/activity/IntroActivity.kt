60f5558f74a07e30a2a03a87558f6062044c0556
Elber Kerim <kerimelber2@gmail.com>
2024-04-05 15:58:43 +0300

    IntroPage and HomePage ui finish

---
+++ app/src/main/java/com/example/e_commerce_app_2/activity/IntroActivity.kt
@@ -0,0 +1,26 @@
+package com.example.e_commerce_app_2.activity
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.e_commerce_app_2.MainActivity
+import com.example.e_commerce_app_2.R
+import com.example.e_commerce_app_2.databinding.ActivityIntroBinding
+
+class IntroActivity : BaseActivity() {
+    private lateinit var binding: ActivityIntroBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityIntroBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button2.setOnClickListener {
+            startActivity(Intent(this@IntroActivity, MainActivity::class.java))
+        }
+
+
+    }
+}
